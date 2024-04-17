f204274f624ce0ee9227f8f4f9ce27f2126425d8
Josué Licona <liconaj215@gmail.com>
2024-04-16 21:52:27 -0500

    Mejora de controller y establecimiento por defecto; parámetro para decidir qué modelo FEM usar; cambio constantes modelos; variables para establecer presiones de entrada

--- controller.py
+++ controller.py
@@ -6,12 +6,14 @@ import pandas as pd
 class Controller(Sofa.Core.Controller):
     def __init__(self, *args, **kwargs):
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
-        self.pressure_1 = kwargs['pressure_cavity_1']
-        self.pressure_2 = kwargs['pressure_cavity_2']
-        self.pressure_3 = kwargs['pressure_cavity_3']
+        self.pressure_cavity_1 = kwargs['pressure_cavity_1']
+        self.pressure_cavity_2 = kwargs['pressure_cavity_2']
+        self.pressure_cavity_3 = kwargs['pressure_cavity_3']
         self.tip_roi = kwargs['tip_roi']
         self.index = 0
-        self.input_pressures_1 = np.linspace(0, 10000, 500)
+        self.input_pressures_1 = kwargs['input_pressures_1']
+        self.input_pressures_2 = kwargs['input_pressures_2']
+        self.input_pressures_3 = kwargs['input_pressures_3']
         self.csv_file = "data.csv"
         with open(self.csv_file, "w", newline="") as f:
             df_header = pd.DataFrame({"1": ["pressure1"], "2": ["pressure2"], "3": ["pressure3"],
@@ -19,22 +21,26 @@ class Controller(Sofa.Core.Controller):
             df_header.to_csv(f, header=False, index=False)
             f.close()
 
-    def onAnimateEndEvent(self, e: dict):
-        if self.index < len(self.input_pressures_1):
-            pressure_1 = self.input_pressures_1[self.index]
-            self.pressure_1.pressure.value = pressure_1
-            self.index += 1
-            print("New Pressure {}".format(self.pressure_1.pressure.value))
-            tip_position_mm = 1000*np.mean(self.tip_roi.position.value, axis=0)
-            print(tip_position_mm)
-            df = pd.DataFrame({
-                "pressure_1": [pressure_1],
-                "pressure_2": [0],
-                "pressure_3": [0],
-                "tipX": tip_position_mm[0],
-                "tipY": tip_position_mm[1],
-                "tipZ": tip_position_mm[2],
-            })
-            with open(self.csv_file, "a", newline="") as f:
-                df.to_csv(f, header=False, index=False)
-                f.close()
+    def update_pressure(self, pressure_cavity, input_pressures):
+        if self.index < len(input_pressures):
+            pressure_cavity.pressure.value = input_pressures[self.index]
+
+    def onAnimateEndEvent(self, _: dict):
+        self.update_pressure(self.pressure_cavity_1, self.input_pressures_1)
+        self.update_pressure(self.pressure_cavity_2, self.input_pressures_2)
+        self.update_pressure(self.pressure_cavity_3, self.input_pressures_3)
+        self.index += 1
+
+        tip_position_mm = 1000*np.mean(self.tip_roi.position.value, axis=0)
+        print(tip_position_mm)
+        df = pd.DataFrame({
+            "pressure_1": [self.pressure_cavity_1.pressure.value],
+            "pressure_2": [self.pressure_cavity_2.pressure.value],
+            "pressure_3": [self.pressure_cavity_3.pressure.value],
+            "tipX": tip_position_mm[0],
+            "tipY": tip_position_mm[1],
+            "tipZ": tip_position_mm[2],
+        })
+        with open(self.csv_file, "a", newline="") as f:
+            df.to_csv(f, header=False, index=False)
+            f.close()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import Sofa
 from dataclasses import dataclass
 from controller import Controller
+import numpy as np
+
+input_pressures_1 = np.linspace(0, 0, 500)
+input_pressures_2 = np.linspace(0, 10e3, 500)
+input_pressures_3 = np.linspace(0, 10e3, 500)
 
 
 @dataclass
@@ -18,11 +23,15 @@ class Models:
 
 @dataclass
 class Properties:
+    gravity: tuple = (0, 0, -9.8)
     mass: float = 15e-3
+    is_hyperelastic: bool = True
+    # Modelo hiperelástico
     material: str = "MooneyRivlin"
-    material_parameter_set: str = "10000 120000 10e5"
+    material_parameter_set: str = "8053.7 2013.4 1e5"
+    # Modelo elástico
     poisson_ratio: float = 0.49
-    young_modulus: float = 100e3
+    young_modulus: float = 80e3
 
 
 def loadPlugins(root: Sofa.Core.Node) -> None:
@@ -43,27 +52,30 @@ def loadPlugins(root: Sofa.Core.Node) -> None:
 
 
 def createScene(root: Sofa.Core.Node) -> Sofa.Core.Node:
-    root.dt.value = 0.05
-    root.gravity.value = [0, 0, -9.8]
+    root.dt.value = 0.01
+    root.gravity.value = Properties.gravity
 
     loadPlugins(root)
 
     # Visualización
     root.addObject("LineAxis", size=0.3)
-    root.addObject("VisualStyle", displayFlags="hideVisual showBehavior")
+    root.addObject("VisualStyle", displayFlags="showVisual showBehavior")
 
     # Loop animación
     root.addObject("DefaultAnimationLoop")
 
     hook = createHook(root, "Hook")
-    cavity_1 = createCavity(hook, "Cavity1", Models.hook_cavity_1_stl, 50000)
+    cavity_1 = createCavity(hook, "Cavity1", Models.hook_cavity_1_stl, 0)
     cavity_2 = createCavity(hook, "Cavity2", Models.hook_cavity_2_stl, 0)
     cavity_3 = createCavity(hook, "Cavity3", Models.hook_cavity_3_stl, 0)
 
-    #hook.addObject(Controller(pressure_cavity_1=cavity_1.CavityPressure,
-    #                          pressure_cavity_2=cavity_2.CavityPressure,
-    #                          pressure_cavity_3=cavity_3.CavityPressure,
-    #                          tip_roi=hook.TipROI))
+    hook.addObject(Controller(pressure_cavity_1=cavity_1.CavityPressure,
+                              pressure_cavity_2=cavity_2.CavityPressure,
+                              pressure_cavity_3=cavity_3.CavityPressure,
+                              input_pressures_1=input_pressures_1,
+                              input_pressures_2=input_pressures_2,
+                              input_pressures_3=input_pressures_3,
+                              tip_roi=hook.TipROI))
 
     return root
 
@@ -75,7 +87,7 @@ def createHook(parent: Sofa.Core.Node, name: str) -> Sofa.Core.Node:
     # Solucionadores
     # hook.addObject("StaticSolver")
     hook.addObject('EulerImplicitSolver', rayleighStiffness=0.1, rayleighMass=0.1)
-    hook.addObject("CGLinearSolver", iterations=100, tolerance=1e-5, threshold=1e-5)
+    hook.addObject("CGLinearSolver", iterations=100, tolerance=1e-8, threshold=1e-14)
 
     # Cargar modelos (mallas)
     volume_mesh = hook.addObject("MeshGmshLoader", name="VolumeMesh", filename=Models.hook_msh, scale=Models.scale)
@@ -91,8 +103,10 @@ def createHook(parent: Sofa.Core.Node, name: str) -> Sofa.Core.Node:
 
     # Propiedades
     hook.addObject("UniformMass", totalMass=Properties.mass)
-    # hook.addObject("TetrahedronHyperelasticityFEMForceField", template="Vec3", name="HyperElasticFEM", materialName=Properties.material, ParameterSet=Properties.material_parameter_set)
-    hook.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=Properties.poisson_ratio, youngModulus=Properties.young_modulus)
+    if Properties.is_hyperelastic:
+        hook.addObject("TetrahedronHyperelasticityFEMForceField", template="Vec3", name="HyperElasticFEM", materialName=Properties.material, ParameterSet=Properties.material_parameter_set)
+    else:
+        hook.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=Properties.poisson_ratio, youngModulus=Properties.young_modulus)
 
     # Soporte fijo
     support_mesh = hook.addObject("MeshSTLLoader", name="SupportMesh", filename=Models.hook_support_stl, scale=Models.scale)
@@ -111,9 +125,9 @@ def createHook(parent: Sofa.Core.Node, name: str) -> Sofa.Core.Node:
                    ROIposition=f"{tip_mesh.getLinkPath()}.position", ROItriangles=f"{tip_mesh.getLinkPath()}.triangles")
 
     # Visualización
-    # visual = hook.addChild("Visual")
-    # visual_model = visual.addObject("OglModel", name="VisualModel", src=visual_mesh.getLinkPath(), color="white")
-    # visual.addObject("BarycentricMapping", input=hook.getLinkPath(), output=visual_model.getLinkPath())
+    visual = hook.addChild("Visual")
+    visual_model = visual.addObject("OglModel", name="VisualModel", src=visual_mesh.getLinkPath(), color="white")
+    visual.addObject("BarycentricMapping", input=hook.getLinkPath(), output=visual_model.getLinkPath())
 
     return hook
 
